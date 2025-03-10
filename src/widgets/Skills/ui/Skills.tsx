01ccfb1d5fe6c056fe0dd92b1dd9a73fe3c80a51
Roman <roman.chaban1001@gmail.com>
2025-03-10 02:47:44 +0200

    Corrected Skills css file.

--- src/widgets/Skills/ui/Skills.tsx
+++ src/widgets/Skills/ui/Skills.tsx
@@ -6,7 +6,7 @@ import { Container } from "@/shared/ui/Container/Container";
 
 import { skillsList } from "../data/skills";
 
-import "./Skills.css";
+import styles from "./Skills.module.css";
 
 const Skills: FC = () => {
    return (
@@ -14,7 +14,9 @@ const Skills: FC = () => {
          tag='section'
          className='bg-skills relative h-full w-full overflow-hidden p-5'
       >
-         <div className='animate-scroll col-span-12 flex w-max py-8'>
+         <div
+            className={`${styles["animate-scroll"]} col-span-12 flex w-max py-8`}
+         >
             <ul className='flex w-full items-center gap-10 px-10'>
                {[...skillsList, ...skillsList].map((item, index) => (
                   <li
@@ -24,7 +26,7 @@ const Skills: FC = () => {
                      <div className='flex h-20 w-20 items-center justify-center'>
                         <Image
                            priority
-                           className='icon'
+                           className={styles.icon}
                            src={item.icon}
                            alt={item.label}
                            width={70}
